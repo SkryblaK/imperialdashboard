1b7d3ff478badf83f72c12deb33dda113af047a0
SkryblaK <georgemtkeneddy@gmail.com>
2026-07-03 11:33:10 +0200

    README Fix

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,8 @@ export default function Page() {
             <div className="hidden md:flex flex-col md:w-1/3 items-center justify-center select-none">
                 {/* <Image src='/logo-light.png' height={400} width={400} alt="Main Attraction" /> */}
                 {/* <Image src='/test/logos/westpac-group.svg' height={400} width={400} alt="Something"/> */}
-                <Image src='/logos/' height={400} width={400} alt="Something" className="" loading="eager"/>
+                <Image src='/logos/logo-dark.png' height={400} width={400} alt="Something" className="not-dark:hidden" loading="eager"/>
+                <Image src='/logos/logo-light-temp.png' height={400} width={400} alt="Something" className="dark:hidden" loading="eager"/>
                 
             </div>
           </div>
